a87a3e5ab200bef7302e82c0a9a1c6e2f19c2ab1
zyp2005 <zyp2005@www.github.com>
2023-06-27 18:46:59 +0800

    happy

---
+++ 2th/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)       /*一个简单的c程序*/
+{
+    int num;        //定义一个名为num的变量
+    num = 1;        //为num赋一个值
+
+    printf("I am a simple ");     //使用printf()函数
+    printf("computer.\n");
+    printf("My favourate number is %d because it is first.\n", num);
+
+    return 0;
+}
